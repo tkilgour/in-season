3217030878d2944fc1d02ec88549f4e7a5eefc95
Thomas Kilgour <t.kilgour@gmail.com>
2017-02-23 16:33:36 -0500

    configure routes and update crops controller

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -1,5 +1,35 @@
 class CropsController < ApplicationController
+  def index
+    @crops = Farm.find(param[:farm_id]).crops
+  end
+
+  def create
+    @crop = Crop.new(crop_params)
+  end
+
   def show
     @crops = Crop.find(param[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @crop = Crop.find(param[:id])
+    @crop.destroy
+    redirect_to farm_crops_path
+  end
+
+  private
+
+  def crop_params
+    params.require(:crop).permit(
+      :farm_id,
+      :name,
+      :image,
+      :description,
+      :harvest_date,
+      :availability
+    )
   end
 end
