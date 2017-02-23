0bb9332afb55d902b18fc30fa10138d9835862cf
Thomas Kilgour <t.kilgour@gmail.com>
2017-02-23 11:06:49 -0500

    add product_templates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222215116) do
+ActiveRecord::Schema.define(version: 20170222225027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170222215116) do
     t.string   "name"
     t.string   "farmers"
     t.string   "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_templates", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
