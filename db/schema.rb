c55b48fcf7e81a6e58254945f4779e97c2e6cb09
Young <mcpjjh@gmail.com>
2017-02-28 20:28:08 +0000

    changed button colors to green, spaced buttons out in modal, added header in modal, added glyphs in modal

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20170228175230) do
     t.string   "banner_image"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.text     "about_farm"
     t.integer  "user_id"
   end
 
