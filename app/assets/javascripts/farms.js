04ce29da4ad7f51019a3f10956ef1126b74909aa
Thomas Kilgour <t.kilgour@gmail.com>
2017-02-28 15:05:37 -0500

    last state of gmaps4rails

--- app/assets/javascripts/farms.js
+++ app/assets/javascripts/farms.js
@@ -1,30 +1,38 @@
-$(document).on('turbolinks:load', function() {
-  handler = Gmaps.build('Google');
-  handler.buildMap(
-    {
-      provider: {
-        disableDefaultUI: true
-        // here you can pass other Google Maps API options here
-      },
-      internal: {
-        id: 'map'
-      }
-    },
-    function() {
-      markers = handler.addMarkers([
-        {
-          "lat": 0,
-          "lng": 0,
-          "picture": {
-            "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
-            "width":  36,
-            "height": 36
-          },
-          "infowindow": "hello!"
+window.FarmMap = (function() {
+  function FarmMap(markers) {
+    this.markers = markers;
+  }
+
+  FarmMap.prototype.init = function() {
+    handler = Gmaps.build('Google');
+    handler.buildMap(
+      {
+        provider: {
+          // disableDefaultUI: true
+          // here you can pass other Google Maps API options here
+        },
+        internal: {
+          id: 'map'
         }
-      ]);
-      handler.bounds.extendWith(markers);
-      handler.fitMapToBounds();
-    }
-  );
-});
+      },
+      function() {
+        // markers = handler.addMarkers([
+        //   {
+        //     "lat": 0,
+        //     "lng": 0,
+        //     "picture": {
+        //       "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+        //       "width":  36,
+        //       "height": 36
+        //     },
+        //     "infowindow": "hello!"
+        //   }
+        // ]);
+        handler.bounds.extendWith(this.markers);
+        handler.fitMapToBounds();
+      }
+    );
+  }
+
+  return FarmMap;
+})();
--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -6,5 +6,11 @@ class FarmsController < ApplicationController
   def show
     @farm = Farm.find(params[:id])
     @crops = @farm.crops
+    @markets = @farm.markets
+
+    @hash = Gmaps4rails.build_markers(@markets) do |market, marker|
+      marker.lat market.lat
+      marker.lng market.lng
+    end
   end
 end
