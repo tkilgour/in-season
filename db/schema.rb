3bbc8b1310342d2cc35c6160581d8f059f5f4d80
Rhea <rhea.morrison@gmail.com>
2017-03-03 18:45:14 +0000

    routing fixes, box page headers with >>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302201944) do
+ActiveRecord::Schema.define(version: 20170303145359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,13 +41,14 @@ ActiveRecord::Schema.define(version: 20170302201944) do
     t.string   "farmer"
     t.string   "profile_image"
     t.string   "banner_image"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "user_id"
     t.string   "address"
     t.float    "lat"
     t.float    "lng"
     t.text     "about_farm"
+    t.boolean  "csa_availability"
   end
 
   create_table "farms_markets", id: false, force: :cascade do |t|
