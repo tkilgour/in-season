b04ce2bf0ada69ac7bd3bd4d4fd5a60f927ff382
Rhea <rhea.morrison@gmail.com>
2017-03-04 23:50:43 +0000

    login error message

---
+++ public/farmers/\_files/mQOmPtj5PWA.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["nOPhW"]); }
+
+__d('TimelineEntStreamContentLoader',['csx','CSS','DOMQuery','ge'],(function a(b,c,d,e,f,g,h){var i={removeDupes:function j(k){var l=c('ge')(k);if(!l)return;var m={},n=[];c('DOMQuery').scry(l,"._5jmm").forEach(function(o){if(o&&o.id&&o.id.startsWith('tl_unit_'))if(m[o.id]){n.push(o);}else m[o.id]=1;});n.forEach(function(o){o.id+='_dupe';c('CSS').hide(o);});}};f.exports=i;}),null);
---
+++ public/farmers/\_files/vQjY4vNEhUY.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["03gJv"]); }
+
+__d('isOculusDotComURI',['URI'],(function a(b,c,d,e,f,g){var h=new RegExp('(^|\\.)oculus\\.com$','i'),i=['https'];function j(k){if(k.isEmpty()&&k.toString()!=='#')return false;if(!k.getDomain()&&!k.getProtocol())return false;return (i.indexOf(k.getProtocol())!==-1&&h.test(k.getDomain()));}f.exports=j;}),null);
