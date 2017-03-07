45465942408fc057920b51ec23f4efc7fe4836dd
Young <mcpjjh@gmail.com>
2017-03-07 20:19:56 +0000

    farm/id/boxes/id pages layout edit with card

--- app/assets/javascripts/add-crop.js
+++ app/assets/javascripts/add-crop.js
@@ -3,8 +3,8 @@
 $(document).ready(function() {
 
   $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectMonths: true,
+    selectYears: 15
   });
 });
 
